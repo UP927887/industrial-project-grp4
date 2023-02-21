0eb5e75392818c3160d9c7b00f8db919a739d187
UP926975 <up926975@myport.ac.uk>
2023-02-21 20:54:54 +0000

    Login Page .jsx and .css Modifications

--- front_end/src/Login.jsx
+++ front_end/src/Login.jsx
@@ -1,9 +1,31 @@
 export const Login = () => {
     return (
-        <div>
-            <form>
+        <div className="login-form-container">
+            <form className="login-form">
                 <fieldset>
-                    <label for="username">Username <input id = "username" name="username" type = "text" required/></label>
+                    <div>
+                        <label for="username">Username</label>
+                        <input 
+                            id = "username" 
+                            name = "username" 
+                            type = "text" required
+                        />
+                    </div>
+
+                    <div>
+                        <label for="password">Password</label>
+                        <input 
+                            id = "password" 
+                            name = "password" 
+                            type = "text" required
+                        />
+                    </div>
+
+                    <div>
+                        <button type="submit">
+                            Submit 
+                        </button>
+                    </div>
                 </fieldset>
             </form>
         </div>
